0ea0ea3a2bf30ece09cb81f6e212ddd562313ec0
ArturDokhno <artur.dohno@icloud.com>
2022-05-01 16:22:27 +0500

    Creating MainTabBarViewController

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  NetflixClone
 //
 //  Created by Артур Дохно on 01.05.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
     }
 
-
 }
-
---
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Артур Дохно on 01.05.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
